b85d961f99041a6ccbd0e762d5e677c0fed76b2e
GalibIbr <galibiavi4@gmail.com>
2023-12-12 13:29:47 +0300

    testimonial

--- src/app/components/Testimonial.js
+++ src/app/components/Testimonial.js
@@ -1,5 +1,12 @@
+// components
+import TestimonialSlider from "./TestimonialSlider";
+
 export default function Testimonial() {
   return (
-    <section className="section flex items-center bg-teal-200" id="testimonial">Testimonial</section>
-  )
+    <section className="section flex items-center" id="testimonial">
+      <div className="container mx-auto">
+        <TestimonialSlider />
+      </div>
+    </section>
+  );
 }
