62d6379e9745ecc14f61c19a3d67d8073f9f63ec
GalibIbr <galibiavi4@gmail.com>
2023-12-12 14:21:14 +0300

    footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,7 +1,92 @@
-import React from 'react'
+"use client";
+
+// next image
+import Image from "next/image";
+
+// icons
+import { FaPhone, FaEnvelope } from "react-icons/fa6";
+
+// components
+import Copyright from "./Copyright";
+
+// motion
+import { motion } from "framer-motion";
+
+// variants
+import { fadeIn } from "../../../variants";
+
+// link / react scroll
+import { Link } from "react-scroll";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="pt-20 bg-white z-20">
+      <div className="container mx-auto mb-24">
+        {/* grid */}
+        <motion.div
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="flex flex-col lg:flex-row lg:justify-between gap-x-5 gap-y-14">
+          <div className="flex flex-col flex-1 gap-y-8">
+            {/* logo */}
+            <Link
+              to={"home"}
+              smooth={true}
+              spy={true}
+              className="cursor-pointer">
+              <Image src={"/icons/logo.svg"} width={200} height={200} alt="" />
+            </Link>
+            {/* text */}
+            <div className="text-secondary">Lorem ipsum dolor sit amet, components adipisicing alit.</div>
+            {/* phone & email */}
+            <div className="flex flex-col gap-y-4 font-semibold">
+              {/* phone */}
+              <div className="flex items-center gap-x-[10px]">
+                <FaPhone />
+                <div className="font-medium">(123)456-7890</div>
+              </div>
+              <div className="flex items-center gap-x-[10px]">
+                <FaEnvelope className="font-medium"/>
+                <div>office@carland.com</div>
+              </div>
+            </div>
+          </div>
+          {/* links */}
+          <div className="flex-1 flex flex-col xl:items-center">
+            <div>
+              <h3 className="h3 font-bold mb-8">Company</h3>
+              <ul className="flex flex-col gap-y-4 font-semibold">
+                <li><a href="">New York</a></li>
+                <li><a href="">Careers</a></li>
+                <li><a href="">Mobile</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">How we work</a></li>
+              </ul>
+            </div>
+          </div>
+          {/* program */}
+          <div className="flex-1">
+            <h3 className="h3 font-bold mb-8">Working Hours</h3>
+            <div className="flex flex-col gap-y-4">
+              <div className="flex gap-x-2">
+                <div className="text-secondary">Mon-Fro:</div>
+                <div className="font-semibold">09:00AM - 09:00PM</div>
+              </div>
+              <div className="flex gap-x-2">
+                <div className="text-secondary">Sat:</div>
+                <div className="font-semibold">09:00AM - 07:00PM</div>
+              </div>
+              <div className="flex gap-x-2">
+                <div className="text-secondary">Sun:</div>
+                <div className="font-semibold">Closed</div>
+              </div>
+            </div>
+          </div>
+          <div>4</div>
+        </motion.div>
+      </div>
+    </footer>
+  );
 }
